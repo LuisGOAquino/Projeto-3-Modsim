07fee060766f4c3a31e46542a08dd6f112db4a9c
anthoni-serro <anthonimcs@al.insper.edu.br>
2025-05-13 11:19:57 -0300

    BOB CHALTON

--- Rascunho.py
+++ Rascunho.py
@@ -1,9 +1,9 @@
 mt = 150                #Massa do conjunto humano + acessorios
-M =  5.972*1e24          #Massa da Terra
-G = 6.67*1e-11           #Constante de gravitação universal
+M =  5.972e24          #Massa da Terra
+G = 6.67e-11           #Constante de gravitação universal
 R = 6371000             #Raio da Terra
-po = 1.225              #PEDIR NOME
-ho = 7500               #PEDIR NOME
+po = 1.225              #Densidade ao nível do mar
+ho = 7500               #Constante de decaimento exponencial
 
 import pandas
 from math import*
@@ -12,42 +12,60 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def modelo(Y, t):
-    y =Y[0]
+    y = Y[0]
     vy = Y[1]
-    if y>= 2455: 
-        A = 0.75     #Área de uma pessoa com os braços abertos
-        Cd= 1.3      #Coeficiente de arrasto do ar para uma pessoa em queda livre
+    if y >= 2560: 
+        A = 1.0     #Área de uma pessoa com os braços abertos
+        Cd= 1.1      #Coeficiente de arrasto do ar para uma pessoa em queda livre
     else:            #Após a abertura do paraquedas
         A = 20       #Área do paraquedas aberto 
         Cd= 2        #Coeficiente de arrasto do ar para o paraquedas
     p = po*(e**(-y/ho))
     dydt = vy
-    dvydt = (p*Cd*A*(vy)**2)/2*mt - (G*M/((R+y)**2))
+    dvydt = (p*Cd*A*(vy**2))/(2*mt) - (G*M/((R+y)**2))
     L = [dydt, dvydt]
     return L
 
 y0 = 38969           #Altura inicial do humano (m)
-v0 = 1.94            #Velocidade inicial (m/s)
+v0 = 0            #Velocidade inicial (m/s)
 Y0 = [y0, v0]        # Condicoes iniciais
 
-dt = 0.001
-tf = 9*60 + 7 
+dt = 1
+tf = 400
 lista_t = np.arange(0, tf, dt)
 
 q = odeint(modelo, Y0, lista_t)
 y_lista = q[:,0]
 vy_lista = abs(q[:,1])
 
-plt.plot(lista_t/60, y_lista)
+# print (y_lista)
+# print (vy_lista)
+plt.plot(lista_t, y_lista)
 plt.title("Altura x Tempo")
-plt.xlabel("Tempo (minutos)")
+plt.xlabel("Tempo (segundos)")
 plt.ylabel("Altura (metros) ")
 plt.grid()
 plt.show()
 
-plt.plot(lista_t/60, vy_lista)
+plt.plot(lista_t, vy_lista)
 plt.title("Velocidade x Tempo")
-plt.xlabel("Tempo (minutos)")
+plt.xlabel("Tempo (segundos)")
 plt.ylabel("Velocidade (m/s)")
 plt.grid()
 plt.show()
+
+
+# po = 1.225              #PEDIR NOME
+# ho = 7500               #PEDIR NOME
+# p = po*(e**(-y/ho))
+
+# a = np.arange(38969, 0, -10)
+
+# for al in a:
+
+# def densi(ly):
+#     r = []
+#     for al in ly:
+#         p = po*(e**(-al/ho))
+
+
