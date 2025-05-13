8b51123e0a0261e13a96777873f0182aa09a56af
LuisGOAquino <luisoliveiraaquino@gmail.com>
2025-05-13 10:08:09 -0300

    passei as coisas do rascunho pra cada arquivo

--- funcoes.py
+++ funcoes.py
@@ -1,16 +1,20 @@
 from parametros import *
+import pandas
 from math import *
+from scipy.integrate import odeint
+import numpy as np
+import matplotlib.pyplot as plt
 
-def modelo(lista,t):
-    y=lista[0]
-    vy=lista[1]
-    dydt=vy
-    p = p0*(e**(-y/h0))
+def modelo(Y, t):
+    y =Y[0]
+    vy = Y[1]
     if y>= 2455: 
         A = 0.75     #Área de uma pessoa com os braços abertos
         Cd= 1.3      #Coeficiente de arrasto do ar para uma pessoa em queda livre
     else:            #Após a abertura do paraquedas
         A = 20       #Área do paraquedas aberto 
         Cd= 2        #Coeficiente de arrasto do ar para o paraquedas
-    dvydt = ((p*Cd*A*(vy)**2)/2*mt) - (G*M/((R+y)**2))
-    return [dydt,dvydt]
+    p = po*(e**(-y/ho))
+    dydt = vy
+    dvydt = (p*Cd*A*(vy)**2)/2*mt - (G*M/((R+y)**2))
+    L = [dydt, dvydt]
--- parametros.py
+++ parametros.py
@@ -1,7 +1,6 @@
 mt = 150                #Massa do conjunto humano + acessorios
 M =  5.972*1e24          #Massa da Terra
 G = 6.67*1e-11           #Constante de gravitação universal
-R = 6371000             #Raio da Terra              
-p0=1.225    
-h0=7500     #ALTITUDE BASE
-
+R = 6371000             #Raio da Terra
+po = 1.225              #PEDIR NOME
+ho = 7500               #PEDIR NOME
